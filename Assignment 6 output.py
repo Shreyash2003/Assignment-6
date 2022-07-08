d4b4154a31ceb0a309f1fd0d56fa37b9e94d5350
Shreyash2003 <104262663+Shreyash2003@users.noreply.github.com>
2022-07-08 23:16:21 +0530

    Add files via upload

---
+++ Assignment 6 output.py
@@ -0,0 +1,43 @@
+C:\Users\shrey\PycharmProjects\pythonProject50\venv\Scripts\python.exe C:/Users/shrey/PycharmProjects/pythonProject50/main.py
+Enter any number: 49
+The number is not a Perfect number!
+Kindly enter alphabets only.
+Enter word/sentence to be checked: My name is Shreyash Raj
+No
+Enter the number of rows: 6
+       1
+      1 1
+     1 2 1
+    1 3 3 1
+   1 4 6 4 1
+  1 5 10 10 5 1
+Yes, The given string is pangram!
+
+['']
+['']
+
+
+Student ID: SV12
+Student Name: $ Jean Garner
+
+Student ID: SV12
+Student Name: $ Jean Garner
+
+Student Name: $ Jean Garner
+Student Class: $ V
+True
+False
+True
+False
+
+Checking whether the said classes are subclasses of the built-in object class or not.
+True
+True
+[[-10, 2, 8], [-7, -3, 10]]
+True
+True
+False
+False
+False
+
+Process finished with exit code 0
